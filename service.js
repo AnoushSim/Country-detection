4b7bc166acbd4f06b27cf45053ec9d6fa6da346f
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2019-04-12 11:43:47 +0400

    updated

--- service.js
+++ service.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const app = (require('express'))();
-const UsersSchema = require('./model');
-const bodyParser = require('body-parser');
-const utils = require('./utils');
 const ipstack = require('ipstack');
+const validator = require('validator')
+const passwordHash = require('password-hash');
+const bodyParser = require('body-parser');
+const UsersSchema = require('./model');
+const utils = require('./utils');
+
+
 
 let ipstack_APIKey = 'dcfb911dd798e214072b90a62941b8c8';
 
@@ -22,9 +26,13 @@ app.post('/registration', (req, res) => {
          user.email = req.body.email;
          user.age = req.body.age;
          user.gender = req.body.gender;
-         user.password = req.body.password;
+         user.password = passwordHash.generate(req.body.password);
+         if(validator.isEmail(user.email)) {
+             users.create(user).then(data => res.send('Created')).catch(err => res.send('Something went wrong'))
+         } else {
+             res.send('Please type valid email address');
+         }
 
-         users.create(user).then(data => res.send('Created')).catch(err => res.send('Something went wrong'))
      })
 });
 
@@ -32,18 +40,21 @@ app.post('/login', (req,res) => {
      ipstack(req.ip, ipstack_APIKey, (err, response) => {
 
          let country = (response && response.country_name) ? response.country_name : 'Unknown';
-         users.findOne({username: req.body.username, password: req.body.password}).then(user => {
-             console.log(user);
-             if(user.country === country) {
-                 res.send({success: true, message: 'Success'})
+         users.findOne({username: req.body.username}).then(user => {
+             if(passwordHash.verify(req.body.password, user.password)) {
+                 if(user.country === country) {
+                     res.send({success: true, message: user.key})
+                 } else {
+                     let text = "It seems you have changed your location. To approve it's you, please click to this link: ";
+                     let url = 'some link that will be clicked in client side and will work "/recover/country" put request';
+                     utils.sendMail(user.email,'Recovery Email', text + url);
+                     res.send({success:false, message: 'Check your email. It seems you have changed your location '})
+                 }
              } else {
-                 let text = "It seems you have changed your location. To approve it's you, please click to this link: ";
-                 let url = 'some link that will be clicked in client side and will work "/recover/country" put request';
-                 utils.sendMail(user.email,'Recovery Email', text + url);
-                 res.send({success:false, message: 'Check your email. It seems you have changed your location '})
+                 res.send('Password is not correct');
              }
          }).catch(err => {
-             res.json({success:false , message:'Username or password is not correct'})
+             res.json({success:false , message:'Username is not correct'})
          })
      })
 });
@@ -52,8 +63,10 @@ app.post('/login', (req,res) => {
 app.put('/recover/country', (req,res) => {
     ipstack(req.ip, ipstack_APIKey, (err, response) => {
         let country = (response && response.country_name) ? response.country_name : 'Unknown';
-        users.updateOne({username: req.body.username, password: req.body.password},{$set: {country: country}}).then(result => {
-            res.json({status:true, message: 'Geolocation updated'})
+        users.updateOne({key: req.body.apikey},{$set: {country: country}}).then(result => {
+            if(result.nModified)
+                res.json({status:true, message: 'Geolocation updated'})
+            else res.json({status:false, message: 'Invalid API Key'})
         }).catch(err => {
             res.json({status:false, message: 'Something went wrong'})
         })
